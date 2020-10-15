9da64184abadb75edb2cb8eeee5a1fc17fc6f984
Omar BELKHODJA <omar.belkhodja@gmail.com>
2020-10-15 11:46:30 +0100

    Fixed warnings on icon component

--- dist/large/ChevronLeftMedium.d.ts
+++ dist/large/ChevronLeftMedium.d.ts
@@ -1,2 +1,2 @@
-declare const _default: "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"8\" height=\"12\"><path d=\"M7.197 10.283L2.914 6l4.283-4.283A1 1 0 105.783.303l-4.99 4.99a1 1 0 000 1.414l4.99 4.99a1 1 0 101.414-1.414z\"/></svg>";
+declare const _default: "<svg xmlns='http://www.w3.org/2000/svg' width='8' height='12'><path d='M7.197 10.283L2.914 6l4.283-4.283A1 1 0 105.783.303l-4.99 4.99a1 1 0 000 1.414l4.99 4.99a1 1 0 101.414-1.414z'/></svg>";
 export default _default;
--- dist/large/DashSmall.d.ts
+++ dist/large/DashSmall.d.ts
@@ -1,2 +1,2 @@
-declare const _default: "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"12\" height=\"12\"><path d=\"M10.99 5H1.01a1 1 0 000 2h9.98a1 1 0 100-2z\"/></svg>";
+declare const _default: "<svg xmlns='http://www.w3.org/2000/svg' width='12' height='12'><path d='M10.99 5H1.01a1 1 0 000 2h9.98a1 1 0 100-2z'/></svg>";
 export default _default;
